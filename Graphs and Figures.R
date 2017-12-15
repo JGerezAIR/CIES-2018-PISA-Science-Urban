1e9706ba510e45b8b7eff74976a3bf6681eb24b2
Julian Gerez <jgerez@air.org>
2017-12-15 15:46:46 -0500

    Cleaned up folders, created code for data viz

---
+++ Graphs and Figures.R
@@ -0,0 +1,6 @@
+# This script prepares each of the figures for CIES 2018 paper: PISA Urban Science (working title)
+
+# Written by Julian Gerez
+
+directory <- "G:/Conferences/CIES-2018-PISA-Science-Urban/"
+
