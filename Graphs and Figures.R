657dc69580ce8651cdd9278e82ec5683eb64f182
Julian Gerez <jgerez@air.org>
2017-12-15 15:56:20 -0500

    Means for percentages

--- Graphs and Figures.R
+++ Graphs and Figures.R
@@ -2,5 +2,18 @@
 
 # Written by Julian Gerez
 
+# Clear workspace
+
+rm(list=ls())
+
+library(ggplot2)
+
 directory <- "G:/Conferences/CIES-2018-PISA-Science-Urban/"
+
+# Percentages
+
+percent <- read.csv(paste0(directory, 'Urban Percentages.csv'))
+
+mean_pct_urban <- mean(percent[["pct_urban"]], na.rm=TRUE)
+mean_pct_noturban <- mean(percent[["pct_noturban"]], na.rm=TRUE)
 
